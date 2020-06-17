b77742d64d327415d6f3d262785d1fce0a82dd92
Jojo Zhang <jojozhang0327@gmail.com>
2020-06-17 17:53:02 -0400

    complete moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,21 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    # start a count = 0
+    count = 0
+    # loop through the arr
+    for item in arr:
+        # if item equal to 0
+        if item == 0:
+            # remove the item and increment count by 1
+            arr.remove(item)
+            count = count + 1
+    # return arr + 0*count
+    arr = arr + [0]*count
+    return arr
 
 
 if __name__ == '__main__':
