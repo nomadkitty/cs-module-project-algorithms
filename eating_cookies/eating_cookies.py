705526be9d49b14c51021f857f79aa114ae8f4b9
Jojo Zhang <jojozhang0327@gmail.com>
2020-06-18 16:11:06 -0400

    optimized single number

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,14 +4,33 @@ Returns: an integer
 '''
 
 
-def eating_cookies(n):
-    # base case
-    if n == 1 or n == 0:
+def eating_cookies(n, cache=None):
+    # check for negative values
+    if n < 0:
+        # getting to a negative number mean we didn't
+        # find one of the ways
+        return 0
+    # base case: when there are no more cookies
+    if n == 0:
+        # getting to 0, means we found a way to eat
+        # our cookies
         return 1
-    if n == 2:
-        return 2
-    # recursively adding combo of n, n-1, n-2
-    return eating_cookies(n-1)+eating_cookies(n-2)+eating_cookies(n-3)
+
+    # if no cache yet, create one
+    if cache is None:
+        cache = {}
+    # check for previously computed answer in our cache
+    if n in cache:
+        # return cache[n]
+        return cache[n]
+
+    # this represents our recursive case
+    # three possible decisions
+    # eat 1 cookie, 2 cookies, or 3 cookies
+    cache[n] = eating_cookies(
+        n-1, cache)+eating_cookies(n-2, cache)+eating_cookies(n-3, cache)
+
+    return cache[n]
 
 
 if __name__ == "__main__":
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,8 @@ Returns: an integer
 '''
 
 
-def single_number(arr):
-    # sort the arr
+def single_number_naive(arr):  # O (n log n)
+    # sort the arr O(n log n)
     arr.sort()
 
     # handle head and tail edge cases:
@@ -16,12 +16,48 @@ def single_number(arr):
         return arr[len(arr)-1]
 
     # loop through the arr to check if the item is not equal to both previous and next item
-    for i in range(1, len(arr)-1):
+    for i in range(1, len(arr)-1):  # O(n)
         prev_item = arr[i-1]
         next_item = arr[i+1]
         item = arr[i]
         if item != prev_item and item != next_item:
             return item
+
+
+def single_number(nums):  # O(n)
+    # keep track of number of times an item occurs in input
+    counts = {}
+    # loop through input list O(n)
+    for num in nums:
+        # if item in counts
+        if num in counts:
+            # remove item
+            del counts[num]
+        # otherwise
+        else:
+            counts[num] = 1
+            # add item
+    for k, v in counts.items():  # O(1)
+        if v == 1:
+            return k
+
+
+def single_number_optimized_2(arr):  # O(n) for time O(1) for space
+    s = set()
+    # use either a dictionary or a set
+    # sets: holding onto unique elements
+    # loop through our arr
+    for x in arr:
+        # for each element
+        # check if it is already in our set
+        # if it is, then that's not our out-element-out
+        if x in s:
+            # remove the element from our set
+            s.remove(x)
+        else:
+            s.add(x)
+    # the odd-element-out will be the only element in the set
+    return list(s)[0]
 
 
 if __name__ == '__main__':
